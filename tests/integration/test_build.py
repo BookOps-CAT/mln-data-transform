fdedd16a20db45110694702bc637316ad05c5948
Charlotte Kostelic <charlottekostelic@bookops.org>
2026-06-16 15:55:01 -0400

    combined integration test files, fixed live tests

--- tests/integration/test_build.py
+++ tests/integration/test_build.py
@@ -1,5 +1,5 @@
 import os
-from typing import Generator
+from typing import Any, Generator
 
 import pytest
 from dotenv import load_dotenv
@@ -7,7 +7,166 @@ from dotenv import load_dotenv
 from mln_data_transform.build import TeacherSetBuilder
 
 
+@pytest.fixture
+def set_test_data() -> dict[str, Any]:
+    return {
+        "copies_of_set": 1,
+        "grade_level": "Pre-K",
+        "language": "eng",
+        "set_title": "Foo Bar Teacher Set",
+        "parts": [
+            {"isbn": "9781234567890", "copies": 1},
+            {"copies": 2, "isbn": "9780987654321"},
+        ],
+        "set_type": "Book Club",
+        "study_program_info": "Arts & Music",
+        "local_genre_term": ["Fiction"],
+        "local_topic_term": ["New York City"],
+    }
+
+
 class TestTeacherSetBuilder:
+    def test_create_teacher_sets(
+        self, set_test_data, today_str, mock_responses, caplog
+    ):
+        builder = TeacherSetBuilder(file="data/foo_bar.csv")
+        teacher_set = builder.create_teacher_set(**set_test_data)
+        set_data = builder.validate_set(teacher_set)
+        set_copies = builder.create_set_copies(set_data)
+        valid_set_copies = builder.validate_set_copies(set_copies)
+        bibs = [i.to_bib() for i in valid_set_copies]
+        field_strings = [str(i) for i in bibs[0].fields]
+        assert len(caplog.records) == 10
+        # should be 2 x the number of components + 6
+        assert [i.msg for i in caplog.records] == [
+            "Creating base teacher set for new set: 'Foo Bar Teacher Set'.",
+            "Validating set.",
+            "Record contains 2 ISBN(s) to query WorldCat.",
+            "ISBN 9781234567890: retrieving brief bib record.",
+            "ISBN 9781234567890: retrieving full bib record (OCLC number: ocn123456789).",
+            "ISBN 9780987654321: retrieving brief bib record.",
+            "ISBN 9780987654321: retrieving full bib record (OCLC number: "
+            "ocn123456789).",
+            "Creating 1 copy/copies of set.",
+            "Creating copy 1 of teacher set: Foo Bar Teacher Set.",
+            "Validating 1 copy/copies of set.",
+        ]
+        assert bibs[0].leader == "00000nac  2200000 a 4500"
+        assert teacher_set.local_genre_term == ["Fiction"]
+        assert teacher_set.local_topic_term == ["New York City"]
+        assert field_strings == [
+            "=001  ",
+            "=003  BookOps",
+            f"=008  {today_str}i20032003xxu\\\\\\\\\\\\\\\\\\\\\\000\\0\\eng\\d",
+            "=091  \\\\$aMLNYC ART$fCLUB$pA$c[SHELF-NUMBER]",
+            "=245  00$aFoo Bar Teacher Set.$nCopy 1 of 1",
+            "=300  \\\\$a3 item(s)",
+            '=500  \\\\$aSet consists of 1 copy of "This is New York", 2 copies of "This is New York".',
+            "=520  \\\\$3This is New York$aFake description of book.",
+            "=520  \\\\$3This is New York$aFake description of book.",
+            "=521  2\\$aPre-K",
+            "=526  8\\$aArts & Music",
+            "=651  \\0$aNew York (N.Y.)",
+            "=655  \\7$aFake genre.$2lcgft",
+            "=690  \\7$aBook Club$2bookops",
+            "=691  \\7$aNew York City$2bookops",
+            "=695  \\7$aFiction$2bookops",
+            "=700  12$aSasek, M.$d1916-1980$tThis is New York$f2003$x9781234567890",
+            "=700  12$aSasek, M.$d1916-1980$tThis is New York$f2003$x9780987654321",
+            "=901  \\\\$amlnyc-bot$bCATBL",
+            "=909  \\\\$aOCLC Holdings Exclusion",
+            "=910  \\\\$aBL",
+            "=949  \\\\$a*b2=8;b3=e;bn=ed;",
+            "=949  \\\\$h10$i[BARCODE]-This is New York$leduls$mm$p0.00$q30010$t252$u-$vLOGDOE/mlnyc-bot",
+            "=949  \\\\$h10$i[BARCODE]-This is New York$leduls$mm$p0.00$q30010$t252$u-$vLOGDOE/mlnyc-bot",
+            "=949  \\\\$h10$i[BARCODE]-This is New York$leduls$mm$p0.00$q30010$t252$u-$vLOGDOE/mlnyc-bot",
+        ]
+
+    def test_create_teacher_sets_enhanced(
+        self, set_test_data, today_str, mock_responses, caplog
+    ):
+        builder = TeacherSetBuilder(file="data/foo_bar.csv")
+        set_test_data["special_formats"] = [
+            {"title": "Cat puppet", "description": "A puppet", "copies": 1}
+        ]
+        teacher_set = builder.create_teacher_set(**set_test_data)
+        set_data = builder.validate_set(teacher_set)
+        set_copies = builder.create_set_copies(set_data)
+        valid_set_copies = builder.validate_set_copies(set_copies)
+        bibs = [i.to_bib() for i in valid_set_copies]
+        field_strings = [str(i) for i in bibs[0].fields]
+        assert len(valid_set_copies[0].components) == 3
+        assert sorted([i.field_245.format_field() for i in valid_set_copies]) == sorted(
+            ["Foo Bar Teacher Set. Copy 1 of 1"]
+        )
+        assert len(bibs) == 1
+        assert field_strings == [
+            "=001  ",
+            "=003  BookOps",
+            f"=008  {today_str}i20032003xxu\\\\\\\\\\\\\\\\\\\\\\\\\\|\\||eng\\d",
+            "=091  \\\\$aMLNYC ART$fCLUB E$pA$c[SHELF-NUMBER]",
+            "=245  00$aFoo Bar Teacher Set.$nCopy 1 of 1",
+            "=300  \\\\$a4 item(s)",
+            '=500  \\\\$aSet consists of 1 copy of "This is New York", 2 copies of "This is New York", 1 Cat puppet(s).',
+            "=520  \\\\$3This is New York$aFake description of book.",
+            "=520  \\\\$3This is New York$aFake description of book.",
+            "=520  \\\\$3Cat puppet$aA puppet.",
+            "=521  2\\$aPre-K",
+            "=526  8\\$aArts & Music",
+            "=651  \\0$aNew York (N.Y.)",
+            "=655  \\7$aFake genre.$2lcgft",
+            "=690  \\7$aBook Club$2bookops",
+            "=691  \\7$aNew York City$2bookops",
+            "=695  \\7$aFiction$2bookops",
+            "=700  12$aSasek, M.$d1916-1980$tThis is New York$f2003$x9781234567890",
+            "=700  12$aSasek, M.$d1916-1980$tThis is New York$f2003$x9780987654321",
+            "=901  \\\\$amlnyc-bot$bCATBL",
+            "=909  \\\\$aOCLC Holdings Exclusion",
+            "=910  \\\\$aBL",
+            "=949  \\\\$a*b2=8;b3=e;bn=ed;",
+            "=949  \\\\$h10$i[BARCODE]-This is New York$leduls$mm$p0.00$q30010$t252$u-$vLOGDOE/mlnyc-bot",
+            "=949  \\\\$h10$i[BARCODE]-This is New York$leduls$mm$p0.00$q30010$t252$u-$vLOGDOE/mlnyc-bot",
+            "=949  \\\\$h10$i[BARCODE]-This is New York$leduls$mm$p0.00$q30010$t252$u-$vLOGDOE/mlnyc-bot",
+            "=949  \\\\$h10$i[BARCODE]-Cat puppet$leduls$mm$p0.00$q30010$t252$u-$vLOGDOE/mlnyc-bot",
+        ]
+
+    def test_create_teacher_sets_mocked_parts(
+        self, mock_teacher_set, today_str, set_test_data
+    ):
+        builder = TeacherSetBuilder(file="data/foo_bar.csv")
+        teacher_set = builder.create_teacher_set(**set_test_data)
+        set_data = builder.validate_set(teacher_set)
+        set_copies = builder.create_set_copies(set_data)
+        valid_set_copies = builder.validate_set_copies(set_copies)
+        bibs = [i.to_bib() for i in valid_set_copies]
+        field_strings = [str(i) for i in bibs[0].fields]
+        assert field_strings == [
+            "=001  ",
+            "=003  BookOps",
+            f"=008  {today_str}i20uu20uuxxu\\\\\\\\\\\\\\\\\\\\\\000\\0\\eng\\d",
+            "=091  \\\\$aMLNYC ART$fCLUB$pA$c[SHELF-NUMBER]",
+            "=245  00$aFoo Bar Teacher Set.$nCopy 1 of 1",
+            "=300  \\\\$a2 item(s)",
+            '=500  \\\\$aSet consists of 1 copy of "Foo", 1 copy of "Baz".',
+            "=520  \\\\$3Foo$aA book.",
+            "=520  \\\\$3Baz$aAnother book.",
+            "=521  2\\$aPre-K",
+            "=526  8\\$aArts & Music",
+            "=650  \\0$aFake subject.",
+            "=655  \\7$aFake genre.$2lcgft",
+            "=690  \\7$aBook Club$2bookops",
+            "=691  \\7$aNew York City$2bookops",
+            "=695  \\7$aFiction$2bookops",
+            "=700  12$aBar$d1980-$tFoo$f20uu$x9781234567890",
+            "=730  02$aBaz$x9780987654321",
+            "=901  \\\\$amlnyc-bot$bCATBL",
+            "=909  \\\\$aOCLC Holdings Exclusion",
+            "=910  \\\\$aBL",
+            "=949  \\\\$a*b2=8;b3=e;bn=ed;",
+            "=949  \\\\$h10$i[BARCODE]-Foo$leduls$mm$p0.00$q30010$t252$u-$vLOGDOE/mlnyc-bot",
+            "=949  \\\\$h10$i[BARCODE]-Baz$leduls$mm$p0.00$q30010$t252$u-$vLOGDOE/mlnyc-bot",
+        ]
+
     def test_create_legacy_sets(self, mock_legacy_set, today_str):
         builder = TeacherSetBuilder(file="data/foo_bar.csv")
         legacy_set = builder.create_legacy_set(bib_id="12345678")
@@ -122,7 +281,7 @@ class TestLiveTeacherSetBuilder:
             "=001  ",
             "=003  BookOps",
             f"=008  {today_str}i20032003xxu\\\\\\\\\\\\\\\\\\\\\\000\\0\\eng\\d",
-            "=091  \\\\$aMLNYC SOC$fCLUB$pA$cINC-1356",
+            "=091  \\\\$aMLNYC SOC$fCLUB$pA$c358",
             "=245  00$aThis is New York by M. Sasek.$nCopy 1 of 7",
             "=300  \\\\$a10 item(s)",
             '=500  \\\\$aSet consists of 10 copies of "This is New York".',
@@ -136,7 +295,7 @@ class TestLiveTeacherSetBuilder:
             "=690  \\7$aBook Club$2bookops",
             "=700  12$aSasek, M.$d1916-1980$tThis is New York$f2003$x9780789308849",
             "=901  \\\\$amlnyc-bot$bCATBL",
-            "=901  \\\\$n33333402207472$oTeacher Set SOC A Book Club Set NYC History - This Is New York 1-4",
+            "=901  \\\\$n33333402207449$oTeacher Set SOC A Book Club Set NYC History - This Is New York 1-1",
             "=909  \\\\$aOCLC Holdings Exclusion",
             "=910  \\\\$aBL",
             "=949  \\\\$a*b2=8;b3=e;bn=ed;",
@@ -151,7 +310,7 @@ class TestLiveTeacherSetBuilder:
             "=949  \\\\$h10$i[BARCODE]-This is New York$leduls$mm$p0.00$q30010$t252$u-$vLOGDOE/mlnyc-bot",
             "=949  \\\\$h10$i[BARCODE]-This is New York$leduls$mm$p0.00$q30010$t252$u-$vLOGDOE/mlnyc-bot",
         ]
-        assert len(caplog.records) == 10
+        assert len(caplog.records) == 17
         assert [i.msg for i in caplog.records] == [
             "Creating base teacher set for 19538471.",
             "Getting bib record from platform for 19538471.",
@@ -161,8 +320,15 @@ class TestLiveTeacherSetBuilder:
             "Record contains 1 ISBN(s) to query WorldCat.",
             "ISBN 9780789308849: retrieving brief bib record.",
             "ISBN 9780789308849: retrieving full bib record (OCLC number: 52510777).",
-            "Creating copies of legacy set: 19538471.",
-            "Validating 7 set copies for 19538471.",
+            "Creating 7 copy/copies of set.",
+            "Creating copy 1 of legacy set: 19538471.",
+            "Creating copy 2 of legacy set: 19538471.",
+            "Creating copy 3 of legacy set: 19538471.",
+            "Creating copy 4 of legacy set: 19538471.",
+            "Creating copy 5 of legacy set: 19538471.",
+            "Creating copy 6 of legacy set: 19538471.",
+            "Creating copy 7 of legacy set: 19538471.",
+            "Validating 7 copy/copies of set.",
         ]
 
     def test_legacy_set_builder_ancient_civ(self, today_str, caplog, live_creds):
@@ -190,11 +356,11 @@ class TestLiveTeacherSetBuilder:
             "=001  ",
             "=003  BookOps",
             f"=008  {today_str}i20152015xxu\\\\\\\\\\\\\\\\\\\\\\000\\0\\eng\\d",
-            "=091  \\\\$aMLNYC SOC$fTOPIC$pC$c1077",
+            "=091  \\\\$aMLNYC SOC$fTOPIC$pC$c1072",
             "=245  00$aAncient Civilizations.$nCopy 1 of 8",
             "=300  \\\\$a8 item(s)",
             '=500  \\\\$aSet consists of 1 copy of "Ancient Rome", 1 copy of "Ancient Mesopotamia", 1 copy of "Ancient Maya", 1 copy of "Ancient India", 1 copy of "Ancient Greece", 1 copy of "Ancient Egypt", 1 copy of "Ancient China", 1 copy of "Ancient Aztecs".',
-            "=520  \\\\$3Ancient Rome$a\"In Ancient Rome, readers discover the history and impressive accomplishments of the ancient Romans, including their military power and feats of engineering. Engaging text provides details on the civilization's history, development, daily life, culture, art, technology, warfare, social organization, and more.\"--Publisher's web site.",
+            "=520  \\\\$3Ancient Rome$aIn Ancient Rome, readers discover the history and impressive accomplishments of the ancient Romans, including their military power and feats of engineering. Text provides details on the civilization's history, development, daily life, culture, art, technology, warfare, social organization, and more.",
             "=520  \\\\$3Ancient Mesopotamia$a\"In Ancient Mesopotamia, readers discover the history and impressive accomplishments of the ancient Mesopotamians, including their extraordinary cultural achievements and technological wonders. Engaging text provides details on the civilization's history, development, daily life, culture, art, technology, warfare, social organization, and more.\"--Publisher's web site.",
             "=520  \\\\$3Ancient Maya$a\"In Ancient Maya, readers discover the history and impressive accomplishments of the Maya people, including their advanced mathematics and massive stone cities. Engaging text provides details on the civilization's history, development, daily life, culture, art, technology, warfare, social organization, and more.\"--Publisher's website.",
             "=520  \\\\$3Ancient India$a\"In Ancient India, readers discover the history and impressive accomplishments of the people of ancient India, including their enduring religions and rich literary traditions. Engaging text provides details on the civilization's history, development, daily life, culture, art, technology, warfare, social organization, and more.\"--Publisher's web site.",
@@ -206,7 +372,9 @@ class TestLiveTeacherSetBuilder:
             "=526  8\\$aSocial Studies",
             "=650  \\0$aMayas$vJuvenile literature.",
             "=650  \\0$aAztecs$vJuvenile literature.",
-            "=651  \\0$aRome$xCivilization$vJuvenile literature.",
+            "=651  \\0$aRome$xCivilization.",
+            "=651  \\0$aRome$xHistory.",
+            "=651  \\0$aRome$xSocial life and customs.",
             "=651  \\0$aIraq$xCivilization$yTo 634$vJuvenile literature.",
             "=651  \\0$aMexico$xCivilization$vJuvenile literature.",
             "=651  \\0$aCentral America$xCivilization$vJuvenile literature.",
@@ -228,7 +396,7 @@ class TestLiveTeacherSetBuilder:
             "=700  12$aAtkins, Marcie Flinchum$tAncient China$f2015$x9781624035364",
             "=700  12$aKenney, Karen Latchana$tAncient Aztecs$f2015$x9781624035357",
             "=901  \\\\$amlnyc-bot$bCATBL",
-            "=901  \\\\$n33333408154173$oTeacher Set SOC C Ancient Civilizations 2-6",
+            "=901  \\\\$n33333408154165$oTeacher Set SOC C Ancient Civilizations 2-1",
             "=909  \\\\$aOCLC Holdings Exclusion",
             "=910  \\\\$aBL",
             "=949  \\\\$a*b2=8;b3=e;bn=ed;",
@@ -241,7 +409,7 @@ class TestLiveTeacherSetBuilder:
             "=949  \\\\$h10$i[BARCODE]-Ancient China$leduls$mm$p0.00$q30010$t252$u-$vLOGDOE/mlnyc-bot",
             "=949  \\\\$h10$i[BARCODE]-Ancient Aztecs$leduls$mm$p0.00$q30010$t252$u-$vLOGDOE/mlnyc-bot",
         ]
-        assert len(caplog.records) == 24
+        assert len(caplog.records) == 32
         assert [i.msg for i in caplog.records] == [
             "Creating base teacher set for 20895133.",
             "Getting bib record from platform for 20895133.",
@@ -250,7 +418,7 @@ class TestLiveTeacherSetBuilder:
             "Validating set.",
             "Record contains 8 ISBN(s) to query WorldCat.",
             "ISBN 9781624035425: retrieving brief bib record.",
-            "ISBN 9781624035425: retrieving full bib record (OCLC number: 891122605).",
+            "ISBN 9781624035425: retrieving full bib record (OCLC number: 911497614).",
             "ISBN 9781624035418: retrieving brief bib record.",
             "ISBN 9781624035418: retrieving full bib record (OCLC number: 904347324).",
             "ISBN 9781624035401: retrieving brief bib record.",
@@ -265,6 +433,14 @@ class TestLiveTeacherSetBuilder:
             "ISBN 9781624035364: retrieving full bib record (OCLC number: 891122570).",
             "ISBN 9781624035357: retrieving brief bib record.",
             "ISBN 9781624035357: retrieving full bib record (OCLC number: 891122602).",
-            "Creating copies of legacy set: 20895133.",
-            "Validating 8 set copies for 20895133.",
+            "Creating 8 copy/copies of set.",
+            "Creating copy 1 of legacy set: 20895133.",
+            "Creating copy 2 of legacy set: 20895133.",
+            "Creating copy 3 of legacy set: 20895133.",
+            "Creating copy 4 of legacy set: 20895133.",
+            "Creating copy 5 of legacy set: 20895133.",
+            "Creating copy 6 of legacy set: 20895133.",
+            "Creating copy 7 of legacy set: 20895133.",
+            "Creating copy 8 of legacy set: 20895133.",
+            "Validating 8 copy/copies of set.",
         ]
